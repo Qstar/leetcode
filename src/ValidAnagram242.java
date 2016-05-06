15c092bfa343a7950424beb48ece94053a7a121a
qstar <davesla2012@hotmail.com>
2016-05-06 14:55:59 +0800

    leetcode

---
+++ src/ValidAnagram242.java
@@ -0,0 +1,26 @@
+public class ValidAnagram242 {
+    public static void main(String[] args){
+        System.out.println(new ValidAnagram242().isAnagram("rat", "cat"));
+    }
+
+    private boolean isAnagram(String s, String t){
+        int ascs[] = new int[255];
+        int asct[] = new int[255];
+        boolean flag = true;
+        char[] sarray = s.toCharArray();
+        char[] tarray = t.toCharArray();
+        for (char aSarray : sarray) {
+            ascs[aSarray]++;
+        }
+        for (char aTarray : tarray) {
+            asct[aTarray]++;
+        }
+        for (int i = 0; i < 255; i++) {
+            if (ascs[i] != asct[i]) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
--- src/common/TreeNode.java
+++ src/common/TreeNode.java
@@ -3,7 +3,7 @@ package common;
 public class TreeNode {
     public TreeNode left;
     public TreeNode right;
-    int val;
+    public int val;
 
     TreeNode(int x){
         val = x;
