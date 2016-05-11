541904c26cbe63631b18b35fb6abdc8b0a8549b2
davesla2012@hotmail.com <qs1189214788>
2016-05-11 03:23:56 -0400

    leetcode

---
+++ src/PowerofThree326.java
@@ -0,0 +1,13 @@
+public class PowerofThree326 {
+    private boolean isPowerOfThree(int n) {
+        return n >= 1 && n == Math.pow(3, Math.round(Math.log(n) / Math.log(3)));
+    }
+
+    public boolean isPowerOfThree1(int n) {
+        return n == 1 || n != 0 && n % 3 == 0 && isPowerOfThree(n / 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new PowerofThree326().isPowerOfThree(20));
+    }
+}
