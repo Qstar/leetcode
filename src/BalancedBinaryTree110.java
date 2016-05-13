2cc3cd643f9d5744a31ac99db2a7e4be0502e06f
davesla2012@hotmail.com <qs1189214788>
2016-05-13 03:13:51 -0400

    leetcode

---
+++ src/BalancedBinaryTree110.java
@@ -0,0 +1,23 @@
+import common.TreeNode;
+
+/**
+ * 1.查找两支的最大深度
+ * 2.相减作比较绝对值是否超过1
+ */
+public class BalancedBinaryTree110 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        int l = height(root.left);
+        int r = height(root.right);
+        return !(l < 0 || r < 0 || Math.abs(l - r) > 1);
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) return 0;
+        int l = height(root.left);
+        int r = height(root.right);
+        if (l < 0 || r < 0 || Math.abs(l - r) > 1) return -1;
+        else return Math.max(l, r) + 1;
+    }
+}
--- src/ReverseVowelsofaString345.java
+++ src/ReverseVowelsofaString345.java
@@ -1,38 +1,34 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ReverseVowelsofaString345 {
     private String reverseVowels(String s) {
-        Set<Character> vowels = new HashSet<>();
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
+        char[] compare = {'a', 'e', 'i', 'o', 'u'};
         char[] str = s.toCharArray();
-        int len = str.length;
+        int len = s.length();
         String reverse = "";
-        int[] pos = new int[len];
+        ArrayList<Integer> pos = new ArrayList<>();
         for (int i = len - 1; i >= 0; i--) {
-            if (vowels.contains(str[i])) {
-                reverse += str[i];
-                pos[len - i - 1] = len - i;
+            for (char aCompare : compare) {
+                if (aCompare == Character.toLowerCase(str[i])) {
+                    reverse += str[i];
+                    pos.add(i);
+                }
             }
         }
-        int lenPos = pos.length;
-        int count = 0;
-        for (int po : pos) {
-            if (po != 0) {
-                str[po] = reverse.charAt(count);
-                count++;
-            }
+
+        for (int i = pos.size() - 1; i >= 0; i--) {
+            str[pos.get(i)] = reverse.charAt(pos.size() - i - 1);
         }
-        return Arrays.toString(str);
+
+        return String.valueOf(str);
     }
 
     public static void main(String[] args) {
         System.out.println(new ReverseVowelsofaString345().reverseVowels("hello"));
         System.out.println(new ReverseVowelsofaString345().reverseVowels("leetcode"));
+        System.out.println(new ReverseVowelsofaString345().reverseVowels("race car"));
+        System.out.println(new ReverseVowelsofaString345().reverseVowels("aA"));
     }
 }
